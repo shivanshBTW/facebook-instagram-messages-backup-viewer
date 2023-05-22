40ff2f35c0908b8e00697d6c64c25ee9a1e8bbeb
Shivansh Tyagi <stshivansh9@gmail.com>
2023-05-22 23:06:50 +0530

    image zoom implemented

--- src/components/MessagesViewer/MessageItem/MessageItem.js
+++ src/components/MessagesViewer/MessageItem/MessageItem.js
@@ -1,13 +1,24 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Grid, useTheme } from '@mui/material'
-import { MessageItemRoot, MessageItemSenderName } from './styled'
+import {
+  MessageItemImage,
+  MessageItemImageContainer,
+  MessageItemLargeImage,
+  MessageItemLargeImageContainer,
+  MessageItemRoot,
+  MessageItemSenderName
+} from './styled'
 import decodeEmojiString from '../../../utils/decodeEmojiString'
 import commonConfig from '../../../config/commonConfig'
+import Dialog from '@mui/material/Dialog'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
 
 function MessageItem (props) {
+  //   let { userId } = useParams()
   let {
-    messageData: { content = '', sender_name = '' } = {},
-    conversationData: { title = '', joinable_mode } = {}
+    messageData: { content = '', sender_name = '', photos = [] } = {},
+    conversationData: { joinable_mode } = {}
   } = props
 
   let theme = useTheme()
@@ -15,6 +26,29 @@ function MessageItem (props) {
   let isGroupMessage = !!joinable_mode
 
   let decodeEmojiStringCallback = useCallback(decodeEmojiString, [])
+
+  const [imageFileArray, setImageFileArray] = useState([])
+  useEffect(() => {
+    Promise.all(
+      photos.map(async (photoData, index) => {
+        let { uri, creation_timestamp } = photoData
+        let imageURI = uri.replace('messages/inbox/', '')
+        let imageFile = await require(`../../../message-data/${imageURI}`)
+        return { uri: imageFile, creation_timestamp }
+      })
+    ).then(photosArray => {
+      setImageFileArray(photosArray)
+    })
+  }, [photos?.length ? photos[0] : undefined])
+  console.log('imageFileArray', imageFileArray)
+
+  const [largeImageData, setLargeImageData] = useState()
+  const handleImageViewerOpen = photoData => () => {
+    setLargeImageData(photoData)
+  }
+  const handleImageViewerClose = () => {
+    setLargeImageData(undefined)
+  }
 
   return (
     <Grid item alignSelf={isUserTheSender ? 'flex-end' : undefined}>
@@ -25,17 +59,50 @@ function MessageItem (props) {
       )}
       <MessageItemRoot
         elevation={3}
-        style={{
-          backgroundColor: !isUserTheSender
-            ? theme.palette.primary.main
-            : undefined,
-          color: !isUserTheSender
-            ? theme.palette.primary.contrastText
+        style={
+          !isUserTheSender
+            ? {
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.primary.contrastText
+              }
             : undefined
-        }}
+        }
       >
-        {decodeEmojiStringCallback(content)}
+        {content && decodeEmojiStringCallback(content)}
+        {imageFileArray.length
+          ? imageFileArray.map((photoData, index) => {
+              let { uri, creation_timestamp } = photoData
+              return (
+                <MessageItemImageContainer key={creation_timestamp}>
+                  <MessageItemImage
+                    src={uri}
+                    alt={decodeEmojiStringCallback(sender_name) + "'s post"}
+                    onClick={handleImageViewerOpen(photoData)}
+                  />
+                </MessageItemImageContainer>
+              )
+            })
+          : null}
       </MessageItemRoot>
+
+      <Dialog
+        open={!!largeImageData}
+        onClose={handleImageViewerClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>
+          {decodeEmojiStringCallback(sender_name)}'s post
+        </DialogTitle>
+        <DialogContent>
+          <MessageItemLargeImageContainer>
+            <MessageItemLargeImage
+              src={largeImageData?.uri}
+              alt={decodeEmojiStringCallback(sender_name) + "'s post"}
+            />
+          </MessageItemLargeImageContainer>
+        </DialogContent>
+      </Dialog>
     </Grid>
   )
 }
--- src/components/MessagesViewer/MessageItem/styled.js
+++ src/components/MessagesViewer/MessageItem/styled.js
@@ -9,3 +9,17 @@ export const MessageItemRoot = styled(Paper)(({ theme }) => ({
 }))
 
 export const MessageItemSenderName = styled(Typography)(({ theme }) => ({}))
+
+export const MessageItemImageContainer = styled('div')(({ theme }) => ({}))
+
+export const MessageItemImage = styled('img')(({ theme }) => ({
+  maxWidth: '100%',
+  maxHeight: '30vh'
+}))
+
+export const MessageItemLargeImageContainer = styled('div')(({ theme }) => ({}))
+
+export const MessageItemLargeImage = styled('img')(({ theme }) => ({
+  width: '100%',
+  maxHeight: '80vh'
+}))
