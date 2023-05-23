385e77706c7beb4f49f29dad19451f79c5c35636
Shivansh Tyagi <stshivansh9@gmail.com>
2023-05-24 02:47:29 +0530

    improved design of pagination bar

--- src/components/MessagesViewer/styled.js
+++ src/components/MessagesViewer/styled.js
@@ -1,24 +1,32 @@
+import { Paper } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import { styled } from '@mui/material/styles'
 
 export const MessagesViewerRoot = styled('div')(({ theme }) => ({
   [theme.breakpoints.up('xs')]: {
-    padding: theme.spacing(1, 3)
+    padding: theme.spacing(1, 3,12),
   },
   [theme.breakpoints.up('sm')]: {
-    padding: theme.spacing(1, 7)
+    padding: theme.spacing(1, 7,12)
   },
   [theme.breakpoints.up('md')]: {
-    padding: theme.spacing(1, 15)
+    padding: theme.spacing(1, 15,12)
   }
 }))
 
-export const MessagesViewerPaginationBarContainer = styled('div')(
+export const MessagesViewerPaginationBarContainer = styled(Paper)(
   ({ theme }) => ({
     padding: theme.spacing(3, 0),
     textAlign: 'center',
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    borderTopWidth: 1,
+    borderTopStyle: 'solid',
+    borderTopColor: theme.palette.primary.contrastText
   })
 )
 
